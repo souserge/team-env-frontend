b1510906913fd4660ed7faaea4bbcb7cdb7f3a1c
Paula <paula.szymaszek@gmail.com>
2020-10-13 19:35:57 +0300

    add info cards

---
+++ components/InfoCard.vue
@@ -0,0 +1,31 @@
+<template>
+    <v-card>
+        <div class="d-flex flex-no-wrap justify-space-between">
+            <v-img
+                src="https://picsum.photos/350/165?random"
+                width="250"
+                class="grey darken-4"
+            ></v-img>
+        <div>
+            <v-card-title class="title">
+                {{title}}
+            </v-card-title>
+            <v-card-text>
+                Organize your event and manage all the stuff that you need in one platform
+            </v-card-text> 
+        </div>
+            
+        </div>
+    </v-card>
+</template>
+
+<script>
+export default {
+    
+
+}
+</script>
+
+<style>
+
+</style>

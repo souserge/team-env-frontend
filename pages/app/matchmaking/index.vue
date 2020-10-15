928ed784cfb0820717b8878696fb8f69d451b994
Paula <paula.szymaszek@gmail.com>
2020-10-15 21:08:17 +0300

    add matchmaking page

--- pages/app/matchmaking/index.vue
+++ pages/app/matchmaking/index.vue
@@ -1,13 +1,41 @@
 <template>
   <div>
-    <p>
-      This is machmaking page
-    </p>
+    <h1 class="display-1 mt-4 mb-4 text-center">
+        We provide tools to help you in your activism
+    </h1>
+    <v-row justify="center">
+      <v-img
+        max-height="250"
+        max-width="350"
+        :src="require('~/assets/images/matchmaking.png')"
+        @error="debugError"
+      ></v-img>
+    </v-row>
+
+    <v-row justify="center">
+        <v-col align="right" cols="6">
+          <v-btn color="primary" x-large @click="goTo('#our-services')">
+            Find help
+          </v-btn>
+        </v-col>
+        <v-col align="left" cols="6">
+          <v-btn color="primary" to="/app" x-large>
+            Give help
+          </v-btn>
+        </v-col>
+      </v-row>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+
+  methods: {
+    debugError(error) {
+      console.log(error);
+    }
+  }
+};
 </script>
 
 <style></style>

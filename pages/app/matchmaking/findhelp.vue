25bec53356ef5da05de5994935ee1308faaa9f22
Paula <paula.szymaszek@gmail.com>
2020-10-19 21:46:33 +0300

    add find help show results

--- pages/app/matchmaking/findhelp.vue
+++ pages/app/matchmaking/findhelp.vue
@@ -34,14 +34,14 @@
         md="4"
         
       >
-        <v-btn color="primary" to="/app/matchmaking/findhelp" x-medium>
+        <v-btn color="primary" to="/app/matchmaking/findhelp" x-medium @click="showVolunteers">
             Search        
         </v-btn>
      </v-col>
     </v-row>
     <v-row>
       <v-btn color="primary" x-medium @click="openAdvancedSearch">
-            Go to advanced search        
+            Advanced search        
         </v-btn>
     </v-row>
   </v-container>
@@ -49,6 +49,30 @@
   <MatchmakingAdvancedSearch v-show="advancedSearchOpened">
   </MatchmakingAdvancedSearch>
   </v-container>
+  <v-container v-show="volunteersOpened" class="volunteers fill-height" fluid style="min-height: 434px">
+        <v-fade-transition mode="out-in">
+          <v-row>
+            <v-col
+              class="d-flex flex-column"
+              cols="12"
+              md="6"
+              v-for="(item, i) in volunteers"
+              :key="i"
+            >
+              <FindHelpUserCard
+                class="flex d-flex flex-column"
+                :name="item.name"
+                :surname="item.surname"
+                :keywords="item.keywords"
+                :description="item.description"
+                :availability="item.availability"
+                :imageurl="item.imageurl"
+              >
+              </FindHelpUserCard>
+            </v-col>
+          </v-row>
+        </v-fade-transition>
+      </v-container>
   </div>
 </template>
 
@@ -57,12 +81,35 @@ export default {
     data(){
     return {
       advancedSearchOpened: false,
-      items: ["Legal meeting", "Recycling Discussion", "Environmental festival"]
+      volunteersOpened:false,
+      items: ["Legal meeting", "Recycling Discussion", "Environmental festival"],
+      volunteers: [
+      {
+          name: "Lucy",
+          surname: "Brown",
+          keywords: ["Social Media Specialist","Local activist"],
+          description: "My name is Lucy and I am 25 y.o. I am working as Social Media Specialist. I am a local actvivist in my neighborhood.",
+          availability: ["Mon-Thu 4PM-8PM", "Fri 8AM-16PM", "Saturdays"],
+          imageurl: "require('~/assets/images/lucy.png')"
+      },
+      {
+          name: "Paul",
+          surname: "Smith",
+          keywords: ["Photo & Video Specialist","Spanish native speaker"],
+          description: "My name is Paul and I am 34 y.o. and I am passionate about photo and video editing.",
+          availability: ["Mon-Wed", "Weekends"],
+          imageurl: "require('~/assets/images/paul.png')"
+      }
+
+      ]
     }
   },
   methods: {
       openAdvancedSearch() {
-        this.advancedSearchOpened = true;
+        this.advancedSearchOpened = !this.advancedSearchOpened;
+      },
+      showVolunteers() {
+        this.volunteersOpened = true;
       }
     }
 }
